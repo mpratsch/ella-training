2223af5b0cc6883074d9bf14e5476c2c337ac96b
Martina Rath <mpratscher@gmail.com>
2021-12-10 19:08:17 +0100

    Add exercise 02

---
+++ 02-exercise/split_and_join.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def split_and_join(line):
+    # write your code here
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
